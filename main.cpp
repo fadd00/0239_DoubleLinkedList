837e4bc671f78e004a28b6befc5858c1f24a07b3
fadd00 <andhikahutama9@gmail.com>
2024-05-02 15:51:58 +0700

    make the new node point to the first node

--- main.cpp
+++ main.cpp
@@ -17,6 +17,22 @@ void addNode(){
     cin>>newNode->noMhs; //Assign value to the data feild of new node
     cout<<"\nEnter the name of the student: ";
     cin>>newNode->name; // assign value to the data feild of new node
+
+    // Insert the new node in the list
+    if (START == NULL || newNode->noMhs <= START ->noMhs){
+
+        if (START != NULL && newNode->noMhs <= START ->noMhs)
+        {
+            cout << "\033[31mDuplicate rull number not allowed\033[0m]"<<endl;
+            return ;
+        }
+        // if the list is empty, make the new node point to the first node
+        newNode->next = START;
+        if (START!= NULL)
+        {
+            START->prev = newNode; 
+        }
+    }
 }
 
 
