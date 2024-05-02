bc1a44909bf214e97a4e138ebde85eec57b8478d
fadd00 <andhikahutama9@gmail.com>
2024-05-02 15:53:14 +0700

    make the new node point to the new node

--- main.cpp
+++ main.cpp
@@ -27,10 +27,10 @@ void addNode(){
             return ;
         }
         // if the list is empty, make the new node point to the first node
-        newNode->next = START;
+        newNode->next = START; // step 3: make the new node point to the first node
         if (START!= NULL)
         {
-            START->prev = newNode; 
+            START->prev = newNode; // step 4: make the new node point to the new node
         }
     }
 }
