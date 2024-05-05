25d230eae5ca1ecdd69c6af9c3c8e1c8af5c79e3
fadd00 <andhikahutama9@gmail.com>
2024-05-05 10:28:03 +0700

    membuat prosedur search data

--- main.cpp
+++ main.cpp
@@ -154,6 +154,24 @@ void revtravese()
     }
     
 }
+void searchData(){
+    if (listEmpty() == true)
+    {
+        cout << "\nList is empty" << endl;
+    }
+    Node *prev, *curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the student whose record you want to ss=earch: " ;
+    int num;
+    cin >> num;
+    if (search(num, &prev, &curr) == false)
+        cout << "\nRecod not found" << endl;
+    else{
+        cout << "\nRecord found" << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nName: " << curr->name <<endl;
+    }
+}
 int main(){
 
 }
