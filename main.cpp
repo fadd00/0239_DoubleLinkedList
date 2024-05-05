62ac846841cb75ce73408a1c6bc9cbba327aaec3
fadd00 <andhikahutama9@gmail.com>
2024-05-05 09:36:03 +0700

    step 1.d

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ void addNode(){
         Node *previous = NULL; // step 1.b : previous node is NULL initially
     }while (current != NULL && current->noMhs < newNode->noMhs) // step 1.c traverse the list to find the correct position
         {
-            
+            previous = current; // step 1.d : move the previous to the current node
         }
 
 }
